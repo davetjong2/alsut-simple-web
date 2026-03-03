03d35252b5affef823a227b9d1b40e5ad3082b8d
Dave Tjong <tjongdave@gmail.com>
2026-03-03 10:38:23 +0700

    Redirect to Kebun Page

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('sawit.kebun', absolute: false));
     }
 
     /**
